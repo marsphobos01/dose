25e3555949584bca6589a8ce9e9cf5117b200ae7
Morgan Bennett <morganbennett100@gmail.com>
2026-07-26 00:10:43 +0100

    Remove Python

--- dose_site/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),  # ← move this first
-    path('admin/', admin.site.urls),
-    path('', include('tracker.urls')),
-]
